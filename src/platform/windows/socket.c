2429b9bf3c96a939fd61f3aeb59a51eda33ee528
gosse123 <gossenahounou@gmail.com>
2026-04-22 10:25:11 -0700

    implementation de la fonction presence

---
+++ src/platform/windows/socket.c
@@ -0,0 +1,67 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#pragma comment(lib,"Ws2_32.lib")//pour indiquer au compilateur d'ajouter la bib ws2_32.lib lors de la compilation
+#define PORT 47272
+#define BROADCAST_IP "255.255.255.255"//address de diffusion
+
+
+// En gros ,cette fonction ci-dessous permet la 1=>de signaler sa presence sur le resau 2=>detecter la presence des autres pc.
+int presence(char *ip,int port_tcp,char *message); 
+/*fonction main:l'entre du code | programe*/
+int main(void)
+{
+    presence("172.16.17.140",PORT,"gosse");
+    return 0;
+}
+
+int presence(char *ip,int port_tcp,char *message){
+
+    char beacon[256];
+    snprintf(beacon,sizeof(beacon), "toole|%s|%d|%s",ip,port_tcp,message);
+    WSADATA Sa;
+    if(WSAStartup(MAKEWORD(2,2),&Sa) != 0){
+        printf("initialisation de la socket a échoué : %d", WSAGetLastError());
+        exit(1);
+    }
+    SOCKET sock_envoi = socket(AF_INET,SOCK_DGRAM,0);
+    if(sock_envoi == INVALID_SOCKET){
+        printf("creation de la socket  a échoué : %d", WSAGetLastError());
+        WSACleanup();
+        exit(1);
+    }
+    //bro windows a par defaut bloque le broadcast donc cette petite manipe nous permet de debloque le broadcast sur windows
+    BOOL broadcastPermission = TRUE;
+    if(setsockopt(sock_envoi,SOL_SOCKET,SO_BROADCAST,(char*)&broadcastPermission,sizeof(broadcastPermission)) == SOCKET_ERROR){
+        printf("permition de broadcast  a échoué : %d", WSAGetLastError());
+        closesocket(sock_envoi);
+        WSACleanup();
+        exit(1);
+    }
+    u_long mode = 1;
+    if(ioctlsocket(sock_envoi,FIONBIO,&mode) == SOCKET_ERROR){
+        printf("erreur d'ecoute: %d",WSAGetLastError());
+        exit(1);
+    }
+
+    struct sockaddr_in infosock_sa;
+    infosock_sa.sin_family = AF_INET;
+    infosock_sa.sin_port = htons(PORT);
+    infosock_sa.sin_addr.s_addr = INADDR_BROADCAST;
+
+
+    if (sendto(sock_envoi, beacon, strlen(beacon), 0, (struct sockaddr *)&infosock_sa, sizeof(infosock_sa)) == SOCKET_ERROR) {
+            printf("sendto() a échoué : %d", WSAGetLastError());
+
+    }
+        
+
+    // comme tu le sais : si tu a ouvert forcer tu vas  Fermeture.ainsi cette fonction permet de liberer les resource.
+    closesocket(sock_envoi);
+    WSACleanup();
+    return 0;
+    }
